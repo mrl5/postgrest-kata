2d4bc199ac91d3e99736e6c72409bd96338dcb31
mrl5 <31549762+mrl5@users.noreply.github.com>
2023-12-17 21:24:22 +0100

    feat: authz + mutable actions

--- migrations/20231217163420_acls.sql
+++ migrations/20231217163420_acls.sql
@@ -1,6 +1,7 @@
 -- "postgres" is ${ADMIN_DB_USER}
 -- "authenticator" is ${DB_USER}
 
+-- anon role
 CREATE ROLE web_anon NOLOGIN;
 
 GRANT web_anon TO authenticator;
@@ -10,3 +11,13 @@ SELECT
     ON TABLES TO web_anon;
 
 GRANT USAGE ON SCHEMA api TO web_anon;
+
+-- rw user
+CREATE ROLE todo_user NOLOGIN;
+GRANT todo_user TO authenticator;
+
+ALTER DEFAULT PRIVILEGES FOR USER postgres IN SCHEMA data GRANT
+SELECT, INSERT, UPDATE, DELETE
+    ON TABLES TO todo_user;
+
+GRANT USAGE ON SCHEMA api TO todo_user;
--- migrations/20231217174124_api.sql
+++ migrations/20231217174124_api.sql
@@ -9,3 +9,5 @@ FROM
     data.todos;
 
 GRANT SELECT ON api.todos TO web_anon;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON api.todos TO todo_user;
